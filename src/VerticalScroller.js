084d078479de101136e66c1932d15084e9b32d7e
mesihtasci <mesih.t@gmail.com>
2022-08-08 23:49:34 +0200

    add navigation entries

--- src/VerticalScroller.js
+++ src/VerticalScroller.js
@@ -2,8 +2,10 @@ import { htmlToElement, debounce } from './utils/helper';
 
 export default class VerticalScroller {
   bullets = [];
+  navigationEntries = [];
   templates = {
     bullet: /*html*/ `<li class="mt-vs__bullet"></li>`,
+    navigationEntry: /*html*/ `<li class="mt-vs__navigation-entry"></li>`,
   };
 
   constructor() {
@@ -12,6 +14,7 @@ export default class VerticalScroller {
     this.touchendY = 0;
     this.activePage = null;
     this.previousPage = null;
+    this.transition = false;
     this.switchPageDebounced = debounce((event) => this.switchPage(event));
     this.init();
   }
@@ -20,13 +23,14 @@ export default class VerticalScroller {
     this.pages = document.querySelectorAll('.mt-vs__content');
     this.activePage = this.pages[0];
     this.container = document.querySelector('.mt-vs__container');
-    this.addBullets();
+    this.addNavigations();
 
     this.container.addEventListener('wheel', this.switchPageDebounced);
 
     window.document.addEventListener('transitionend', (event) => {
       if (event.target && event.target.classList.contains('mt-vs__content')) {
         this.container.addEventListener('wheel', this.switchPageDebounced);
+        this.transition = false;
       }
     });
 
@@ -43,29 +47,56 @@ export default class VerticalScroller {
     });
   }
 
-  addBullets() {
+  addNavigations() {
     for (let i = 0; i < this.pages.length; i++) {
+      const title = this.pages[i].getAttribute('aria-label');
       const bullet = htmlToElement(this.templates.bullet);
+      const navigationEntry = htmlToElement(this.templates.navigationEntry);
+      navigationEntry.innerHTML = title;
 
       bullet.addEventListener('click', () => {
         this.scrollTo(i);
       });
 
-      if (i === 0) bullet.classList.add('mt-vs__bullet--active');
+      navigationEntry.addEventListener('click', () => {
+        this.scrollTo(i);
+      });
+
+      if (i === 0) {
+        bullet.classList.add('mt-vs__bullet--active');
+        navigationEntry.classList.add('mt-vs__navigation-entry--active');
+      }
 
       this.bullets.push(this.container.querySelector('.mt-vs__side-navigation ul').insertAdjacentElement('beforeend', bullet));
+      this.navigationEntries.push(this.container.querySelector('.mt-vs__top-navigation ul').insertAdjacentElement('beforeend', navigationEntry));
     }
   }
 
-  setActiveBullet(activeIndex) {
+  setActiveNavigationEntry(activeIndex, targetPage) {
     this.bullets.forEach((bullet, index) => {
-      if (index !== activeIndex) bullet.classList.remove('mt-vs__bullet--active');
-      else bullet.classList.add('mt-vs__bullet--active');
+      const navigationEntry = this.navigationEntries[index];
+
+      if (typeof targetPage === 'undefined') {
+        if (index !== activeIndex) {
+          bullet.classList.remove('mt-vs__bullet--active');
+          navigationEntry.classList.remove('mt-vs__navigation-entry--active');
+        } else {
+          bullet.classList.add('mt-vs__bullet--active');
+          navigationEntry.classList.add('mt-vs__navigation-entry--active');
+        }
+      } else if (targetPage === index) {
+        bullet.classList.add('mt-vs__bullet--active');
+        navigationEntry.classList.add('mt-vs__navigation-entry--active');
+      } else {
+        bullet.classList.remove('mt-vs__bullet--active');
+        navigationEntry.classList.remove('mt-vs__navigation-entry--active');
+      }
     });
   }
 
-  setPage(addend) {
+  setPage(addend, targetPage) {
     this.container.removeEventListener('wheel', this.switchPageDebounced);
+    this.transition = true;
     if ((this.currentPage < this.pages.length - 1 && addend === 1) || (this.currentPage > 0 && addend === -1)) {
       this.pages[this.currentPage].classList.remove('mt-vs__content--active');
       this.currentPage = this.currentPage + addend;
@@ -81,11 +112,16 @@ export default class VerticalScroller {
       }
 
       this.activePage.scroll(0, 0);
-      this.setActiveBullet(this.currentPage);
-    } else this.container.addEventListener('wheel', this.switchPageDebounced);
+      this.setActiveNavigationEntry(this.currentPage, targetPage);
+    } else {
+      this.container.addEventListener('wheel', this.switchPageDebounced);
+      this.transition = false;
+    }
   }
 
   scrollTo(targetPage) {
+    if (this.transition) return;
+
     if (this.currentPage === targetPage) return;
 
     const difference = Math.abs(targetPage - this.currentPage);
@@ -101,7 +137,7 @@ export default class VerticalScroller {
       setTimeout(() => {
         if (i === difference - 1) this.container.style.setProperty('--transition-speed', '0.5s');
 
-        this.setPage(addend);
+        this.setPage(addend, targetPage);
       }, i * (transitionSpeed * 1000 + 50));
     }
   }
