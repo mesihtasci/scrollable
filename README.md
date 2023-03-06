8d96121f4d3750adc091cd3ccaa24f2cc489d2af
mesihtasci <mesih.t@gmail.com>
2023-03-06 11:22:40 +0100

    update config

--- README.md
+++
@@ -1,1 +0,0 @@
-# best-man-page
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import { htmlToElement } from './utils/helper.js';
 import { Direction } from './utils/enums.js';
+import './styles.css'
 
-export default class VerticalScroller {
+export default class Scrollable {
   touchStartY = 0;
   touchEndY = 0;
   nextIndex = 0;
+  direction: Direction = Direction.Down;
   pages: HTMLElement[] | null = null;
   currentPageIndex: number | undefined = 0;
   container: HTMLElement | null = null;
   activePage: HTMLElement | null = null;
+
   isTransitionActive = false;
   bullets: HTMLElement[] = [];
   navigationEntries: HTMLElement[] = [];
@@ -22,6 +25,12 @@ export default class VerticalScroller {
   }
 
   init() {
+    const previousSideNavigationElements = document.querySelectorAll('.mt-vs__bullet-wrapper');
+
+    previousSideNavigationElements.forEach((previousElement) => {
+      previousElement.remove();
+    })
+
     const sectionName = decodeURIComponent(location.hash.substring(1));
     this.isTransitionActive = false;
     this.pages = [...document.querySelectorAll('.mt-vs__content')] as HTMLElement[];
@@ -33,10 +42,13 @@ export default class VerticalScroller {
     this.touchEndY = 0;
     if (this.nextIndex < 0) this.nextIndex = 0;
 
+    this.addIndex();
+
     this.addNavigations();
 
     this.container?.addEventListener('wheel', (event) => {
-      this.slideRequest(event.deltaY > 0 ? Direction.Down : Direction.Up);
+      this.direction = event.deltaY > 0 ? Direction.Down : Direction.Up
+      this.slideRequest();
     });
 
     this.container?.addEventListener('touchstart', e => {
@@ -46,31 +58,61 @@ export default class VerticalScroller {
     this.container?.addEventListener('touchend', e => {
       this.touchEndY = e.changedTouches[0].screenY
 
-      if(Math.abs(this.touchStartY - this.touchEndY) < 15)
-      return;
+      if (Math.abs(this.touchStartY - this.touchEndY) < 15)
+        return;
 
-      this.slideRequest(this.touchEndY > this.touchStartY ? Direction.Up : Direction.Down);
+      this.direction = this.touchEndY > this.touchStartY ? Direction.Up : Direction.Down;
+
+      this.slideRequest();
 
       this.touchEndY = 0;
       this.touchStartY = 0;
     })
 
-    this.container?.addEventListener('transitionend', () => {
-      this.isTransitionActive = false;
+    this.container?.addEventListener('transitionstart', (event) => {
+      const eventTaret = event.target as HTMLElement;
+      if (event.propertyName === "transform" && event.target && eventTaret.classList.contains("mt-vs__container")) {
+        this.emitEvent("mt-vt:transition-start", {
+          currentSlideIndex: (this.currentPageIndex! + (this.direction === Direction.Down ? -1 : 1)),
+          nextSlideIndex: this.currentPageIndex,
+          direction: this.direction === Direction.Down ? "down" : "up"
+        });
+      }
+    })
+
+    this.container?.addEventListener('transitionend', (event) => {
+      const eventTaret = event.target as HTMLElement;
+      if (event.propertyName === "transform" && event.target && eventTaret.classList.contains("mt-vs__container")) {
+        this.isTransitionActive = false;
+        this.emitEvent("mt-vt:transition-end", {
+          currentSlideIndex: (this.currentPageIndex! + (this.direction === Direction.Down ? -1 : 1)),
+          nextSlideIndex: this.currentPageIndex,
+          direction: this.direction === Direction.Down ? "down" : "up"
+        });
+      }
     });
 
     if (this.nextIndex >= 0) this.setActivePage(this.nextIndex);
   }
 
-  slideRequest(direction: Direction) {
+  addIndex() {
+    if (this.pages) {
+      this.pages.forEach((page, index) => {
+        page.setAttribute("data-index", index.toString());
+      })
+    }
+  }
+
+  slideRequest() {
     let switchPage = false;
     if (this.activePage) {
+      const activePageHeight = this.activePage.getBoundingClientRect().height;
       if (
-        this.activePage.scrollHeight === window.innerHeight ||
-        (this.activePage.scrollHeight > window.innerHeight &&
-          ((this.activePage.scrollHeight - this.activePage.scrollTop === window.innerHeight &&
-            direction === Direction.Down) ||
-            (this.activePage?.scrollTop === 0 && direction === Direction.Up)))
+        this.activePage.scrollHeight === activePageHeight ||
+        (this.activePage.scrollHeight > activePageHeight &&
+          ((this.activePage.scrollHeight - this.activePage.scrollTop === activePageHeight &&
+            this.direction === Direction.Down) ||
+            (this.activePage?.scrollTop === 0 && this.direction === Direction.Up)))
       ) {
         switchPage = true;
       }
@@ -78,13 +120,17 @@ export default class VerticalScroller {
       this.nextIndex = this.currentPageIndex || 0;
 
       if (switchPage && typeof this.currentPageIndex !== "undefined" && !isNaN(this.currentPageIndex) && this.pages) {
-        if (direction === Direction.Up && this.currentPageIndex > 0) this.nextIndex--;
-        else if (direction === Direction.Down && this.currentPageIndex < this.pages.length - 1) this.nextIndex++;
+        if (this.direction === Direction.Up && this.currentPageIndex > 0) this.nextIndex--;
+        else if (this.direction === Direction.Down && this.currentPageIndex < this.pages.length - 1) this.nextIndex++;
 
         this.setActivePage(this.nextIndex);
 
       }
     }
+  }
+
+  emitEvent(name: string, detail: any) {
+    return window.document.dispatchEvent(new CustomEvent(name, { bubbles: true, cancelable: true, detail: { ...detail } }))
   }
 
   addNavigations() {
@@ -145,15 +191,28 @@ export default class VerticalScroller {
     }
   }
 
+  getYTranslationValue(index: number) {
+    if (this.pages) {
+      let sum = 0;
+
+      for (let i = index; i > 0; i--)
+        sum += this.pages[i].getBoundingClientRect().height;
+
+      return sum;
+    }
+
+    return 0;
+  }
+
   setActivePage(index: number) {
     if (!this.isTransitionActive && this.pages && this.container && this.currentPageIndex !== index && (index < this.pages.length || index >= 0)) {
       if (this.currentPageIndex !== -1 || index > 0)
         this.isTransitionActive = true;
 
-      const yTranslation = index * 100;
+      const yTranslation = this.getYTranslationValue(index);
       this.currentPageIndex = index;
       this.activePage = this.pages[index] as HTMLElement;
-      this.container.style.transform = `translate3d(0, -${yTranslation}vh, 0)`;
+      this.container.style.transform = `translateY(-${yTranslation}px)`;
       this.setActiveNavigationEntry(index);
       const sectionId = this.pages[index].getAttribute('data-section-id');
 
